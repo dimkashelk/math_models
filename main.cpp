46176e3340e0b47ee880b2d23b838dbdac328865
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-04-16 01:14:09 +0300

    add get_x2 func

--- main.cpp
+++ main.cpp
@@ -47,18 +47,39 @@ std::vector< double > get_roots_cubic_equation(double a, double b, double c, dou
   }
   return res;
 }
-double get_a(double p1, double p2, double p3, double p4) {
+double get_a(double p1, double p2, double p3, double p4)
+{
   return std::pow(p1, 2) * p2 * p4 - p1 * p3 * p4 + p1 * p2 * p3 * std::pow(p4, 2) - p2 * p3 * std::pow(p4, 2);
 }
-double get_b(double p1, double p3, double p4) {
+double get_b(double p1, double p3, double p4)
+{
   return 2 * p1 - p3 * p4 + 1;
 }
-double get_c(double p1, double p2, double p3, double p4) {
+double get_c(double p1, double p2, double p3, double p4)
+{
   return 2 * std::pow(p1, 2) - 3 * p1 + 2 * p1 * p3 * p4 + 2 * p1 * p2 * p4 + p2 * p3 * std::pow(p4, 2) - 1;
+}
+double get_x2(double p1, double p2, double p4, double x1) {
+  if (is_equal(p1, x1)) {
+    throw std::runtime_error("Check parameters");
+  }
+  return (p2 * p4 * x1 + x1 * x1 - x1) / (p1 - x1);
 }
 int main()
 {
   double p1 = 8.4E-6, p2 = 6.6667E-4, p3 = 1.7778E-5, p5 = 2;
-
+  for (double p4 = 1.0; p4 <= 2.05; p4 += 0.1)
+  {
+    double parameter = (-1 - p4);
+    double a = -2 * parameter;
+    double b = get_b(p1, p3, p4) * parameter + p5;
+    double c = get_c(p1, p2, p3, p4) * parameter - 2 * p1 * p5;
+    double d = get_a(p1, p2, p3, p4) * parameter + p1 * p1 * p5;
+    auto res = get_roots_cubic_equation(a, b, c, d);
+    std::cout << p4 << ":\n";
+    for (auto i: res) {
+      std::cout << "\t" << i << "\n";
+    }
+  }
   return 0;
 }
